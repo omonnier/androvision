711da5dad7f0aac7d5b3754ef159f32304c5384c
Olivier Monnier <olivier.monnier@intel.com>
2017-01-09 13:24:10 +0100

    Update to selfdriving.py to load a pre-trained model from pickle
    Lots of hardcoded stuff. Just for trial.
    
    Signed-off-by: Olivier Monnier <olivier.monnier@intel.com>

--- misc/RosNodes/selfdriving/scripts/selfdriving.py
+++ misc/RosNodes/selfdriving/scripts/selfdriving.py
@@ -5,6 +5,7 @@ import rospy
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Vector3
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.externals import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
 
@@ -39,34 +40,45 @@ class PredictModel(SensorListener):
         if model_to_use is None:
             model_to_use = DecisionTreeRegressor()
         self._model = model_to_use
+        self._scaler = None
         self._dataset_file = dataset_file
         self._command_publisher = None
         self._feature_columns = None
         # Build the model
-        self.build_model()
+        self.load_model()
 
-    def build_model(self):
-        # Build the model
-        df = pd.read_csv(self._dataset_file, index_col=0)
-        target_columns = constants.ANGULAR_KEY
-        feature_data = df.drop(target_columns, axis=1)
-        x = feature_data.values.astype(float)
-        self._feature_columns = feature_data.columns
-        y = df[target_columns].values.astype(float)
-        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
-        self._model.fit(x_train, y_train)
-        print("Model accuracy: {}".format(self._model.score(x_test, y_test)))
+    def load_model(self):
+        self._scaler = joblib.load('scaler.pkl')
+        self._model = joblib.load('model.pkl')
+        # hack: need to hardcode the feature used to train the model
+        self._feature_columns = ["/left", "/right"]
 
     def init(self):
         SensorListener.init(self)
         # Init publisher to send predicted command
         self._command_publisher = rospy.Publisher(constants.VELOCITY_ACTION_TOPIC, Twist, queue_size=10)
 
+    def X_scaling(self, scaler, values):
+        # add dummy y value
+        values.add(0.)
+        scaled_values = scaler.transform(values)
+        # remove y scaled value
+        return scaled_values[:-1]
+
+    def Y_inverse_scaling(self, scaler, value):
+        # add dummy X values for left, right, delta
+        values = [0., 0., 0.]  + value
+        return scaler.inverse_transform(values)[-1]
+
     def sonar_callback(self, data):
         SensorListener.sonar_callback(self, data)
         if self._command_publisher:
             feature_values = [self._sensor_values.get(x) for x in self._feature_columns if x in self._sensor_values]
+            # hack: the model was trained with the delta: left - right
+            feature_values.append(self._sensor_values.get("/left") - self._sensor_values.get("/right"))
+            feature_values = self.X_scaling(feature_values)
             predict_val = self._model.predict([feature_values])[0]
+            predict_val = self.Y_inverse_scaling(predict_val)
             linear_x = 1 if predict_val != 0 else 0
             cmd_vel = Twist(Vector3(linear_x, 0, 0), Vector3(0, 0, predict_val))
             self._command_publisher.publish(cmd_vel)
@@ -79,4 +91,3 @@ if __name__ == '__main__':
         rospy.spin()
     except KeyboardInterrupt:
         print("Shutting down")
-
